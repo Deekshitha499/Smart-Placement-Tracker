b8f113ef46bb8e8c764b680e56569596e26ca8d8
Deekshitha Gorugantala <deekshithagorugantala@email.com>
2026-05-30 17:44:21 +0530

    Update API URL for production

--- Frontend/src/apis/axios.js
+++ Frontend/src/apis/axios.js
@@ -1,17 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
-})
-
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem('token')
-
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`
-  }
-
-  return req
+  baseURL: import.meta.env.VITE_API_URL
 })
 
 export default API
