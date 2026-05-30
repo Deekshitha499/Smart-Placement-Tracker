28bccc8fd48cb297facf1bee59487f70c3ac3f03
Deekshitha Gorugantala <deekshithagorugantala@email.com>
2026-05-30 17:51:54 +0530

    Update server configuration

--- Backend/server.js
+++ Backend/server.js
@@ -5,33 +5,37 @@ require("dotenv").config();
 const connectDB = require("./config/db");
 
 const studentRoutes = require("./routes/studentRoutes");
+const atsRoutes = require("./routes/atsRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 const errorHandler = require("./middlewares/errorMiddleware");
 
-const atsRoutes = require("./routes/atsRoutes");
-
 const app = express();
 
+// Connect Database
 connectDB();
 
+// Middleware
 app.use(cors());
-
 app.use(express.json());
 
+// Health Check Route
+app.get("/", (req, res) => {
+  res.json({
+    message: "Smart Placement Tracker Backend Running",
+  });
+});
+
+// Routes
+app.use("/api/auth", authRoutes);
 app.use("/api/students", studentRoutes);
-
-app.use(errorHandler);
-
 app.use("/api/ats", atsRoutes);
+
+// Error Handler (Always Last)
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
-
-app.use(
-  "/api/auth",
-  require("./routes/authRoutes")
-);
-
